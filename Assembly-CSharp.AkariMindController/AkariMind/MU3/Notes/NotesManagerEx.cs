eecf5aee1009ee5ac6f0e3438b50735c64e04476
MikiraSora <mikirasora0409@126.com>
2022-08-26 14:22:18 +0800

    rename and add getStartPlayFrame()/getStartNoteFrame()

---
+++ Assembly-CSharp.AkariMindController/AkariMind/MU3/Notes/NotesManagerEx.cs
@@ -0,0 +1,36 @@
+﻿using MonoMod;
+using MU3.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AkariMindControllers.AkariMind.MU3.Notes
+{
+    [MonoModPatch("global::MU3.Notes.NotesManager")]
+    internal class NotesManagerEx : NotesManager
+    {
+        private NoteControlList _noteControlList;
+        private float _curFrame;
+        private NotesNodeCache _notesCache;
+
+        private float _frameNoteStart;
+        private float _framePlayStart;
+
+        public float getStartPlayFrame() => _framePlayStart;
+        public float getStartNoteFrame() => _frameNoteStart;
+
+        public void refreshNotesVisible()
+        {
+            for (int i = 0; i < _noteControlList.Count; i++)
+            {
+                NoteControl noteControl = _noteControlList[i];
+                if (!noteControl.isPlay && !noteControl.isEnd && _curFrame >= noteControl.frameCreate)
+                {
+                    LinkedListNode<NotesBase> note = noteControl.createNotesBase(_notesCache);
+                    addNotesBase(note);
+                }
+            }
+        }
+    }
+}
