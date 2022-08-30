be268d45cbac9cc90f433ae4e5c6924efd317277
MikiraSora <mikirasora0409@126.com>
2022-08-31 04:21:00 +0800

    implement extra menu items to editor

---
+++ OngekiFumenEditorPlugins.AkariMindController/Modules/OngekiGamePlayControllerViewer/ExtraEditorMenuItems/PlayOrPauseMenuItem.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using OngekiFumenEditor.Modules.FumenVisualEditor;
+using OngekiFumenEditor.Modules.FumenVisualEditor.Base;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+using OngekiFumenEditorPlugins.AkariMindController.Modules.OngekiGamePlayControllerViewer.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditorPlugins.AkariMindController.Modules.OngekiGamePlayControllerViewer.ExtraEditorMenuItems
+{
+    [Export(typeof(IFumenVisualEditorExtraMenuItemHandler))]
+    public class PlayOrPauseMenuItem : IFumenVisualEditorExtraMenuItemHandler
+    {
+        public string[] RegisterMenuPath { get; } = new[] { IFumenVisualEditorExtraMenuItemHandler.COMMON_EXT_MENUITEM_ROOT, "AkariMindController", "播放/暂停" };
+
+        public async void Handle(FumenVisualEditorViewModel editor, EventArgs args)
+        {
+            var controller = IoC.Get<IOngekiGamePlayControllerViewer>();
+            if ((await controller.GetNotesManagerData()) is not NotesManagerData data)
+                return;
+        }
+    }
+}
---
+++ OngekiFumenEditorPlugins.AkariMindController/Modules/OngekiGamePlayControllerViewer/ExtraEditorMenuItems/SeekToHereMenuItem.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using OngekiFumenEditor.Modules.FumenVisualEditor;
+using OngekiFumenEditor.Modules.FumenVisualEditor.Base;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditorPlugins.AkariMindController.Modules.OngekiGamePlayControllerViewer.ExtraEditorMenuItems
+{
+    [Export(typeof(IFumenVisualEditorExtraMenuItemHandler))]
+    public class SeekToHereMenuItem : IFumenVisualEditorExtraMenuItemHandler
+    {
+        public string[] RegisterMenuPath { get; } = new[] { IFumenVisualEditorExtraMenuItemHandler.COMMON_EXT_MENUITEM_ROOT, "AkariMindController", "控制游戏跳转到这里" };
+
+        public async void Handle(FumenVisualEditorViewModel editor, EventArgs args)
+        {
+            var controller = IoC.Get<IOngekiGamePlayControllerViewer>();
+            var curTGrid = editor.GetCurrentJudgeLineTGrid();
+            var msec = TGridCalculator.ConvertTGridToAudioTime(curTGrid, editor);
+
+            await controller.SeekTo(msec);
+        }
+    }
+}
